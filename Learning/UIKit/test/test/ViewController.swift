b2b03b903c9084b819ab07d39a398b28fc1424dd
Nadya Tyandra <nadyatyandra05@gmail.com>
2023-05-21 02:23:17 +0700

    initial timer

--- Learning/UIKit/test/test/ViewController.swift
+++ Learning/UIKit/test/test/ViewController.swift
@@ -27,6 +27,45 @@ class ViewController: UIViewController {
         print("Hello")
     }
     
+    // timer
+    var time = 10
+    var timer = Timer()
+    
+    @IBOutlet weak var lbl: UILabel?
+    
+    @IBAction func start(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.action), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        timer.invalidate()
+        time = 10
+        lbl?.text = "10"
+    }
+    
+    @objc func action() {
+        time -= 1
+//        lbl?.text = String(time)
+        lbl?.text = timeFormatted(time)
+        
+        if time == 4 {
+            print("4")
+            timer.invalidate()
+            lbl?.text = "Go!"
+        }
+    }
+    
+    func timeFormatted(_ totalSeconds: Int) -> String {
+        let milliseconds: Int = (totalSeconds * 1000) / 1000
+        let seconds: Int = totalSeconds % 60
+        let minutes: Int = (totalSeconds / 60) % 60
+        return String(format: "%02d:%02d.%03d", minutes, seconds, milliseconds)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
