f6892d423f52c5d362be29c8471f5bcf582f4ab8
Beni Garcia <benigarcia853@gmail.com>
2023-06-22 11:18:58 +0700

    create puzzle lock

---
+++ Mini2/PuzzleLock.swift
@@ -0,0 +1,74 @@
+//
+//  PuzzleLock.swift
+//  Mini2
+//
+//  Created by beni garcia on 21/06/23.
+//
+
+import SpriteKit
+
+class PuzzleLock: SKScene{
+    
+    var lockNumbers: [SKSpriteNode] = []
+    var arrowUpLocks: [SKSpriteNode] = []
+    var arrowDownLocks: [SKSpriteNode] = []
+    var confirmLock: SKSpriteNode?
+    
+    override func didMove(to view: SKView) {
+        for i in 0..<4 {
+            let lockNumber = childNode(withName: "lockNumber\(i)") as? SKSpriteNode
+            let arrowUpLock = childNode(withName: "arrowUpLock\(i)") as? SKSpriteNode
+            let arrowDownLock = childNode(withName: "arrowDownLock\(i)") as? SKSpriteNode
+            
+            lockNumbers.append(lockNumber!)
+            arrowUpLocks.append(arrowUpLock!)
+            arrowDownLocks.append(arrowDownLock!)
+        }
+        confirmLock = childNode(withName: "confirmLock") as? SKSpriteNode
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            for i in 0..<4{
+                if let touchedNode = atPoint(location) as? SKSpriteNode, touchedNode == arrowUpLocks[i] {
+                    
+                    for j in 0..<10{
+                        if lockNumbers[i].texture?.description.components(separatedBy: "'")[1] == "lock9"{
+                            lockNumbers[i].texture = SKTexture(imageNamed: "lock0")
+                            break
+                        }else if lockNumbers[i].texture?.description.components(separatedBy: "'")[1] == "lock\(j)"{
+                            lockNumbers[i].texture = SKTexture(imageNamed: "lock\(j+1)")
+                            break
+                        }
+                    }
+                }
+            }
+            
+            for i in 0..<4{
+                if let touchedNode = atPoint(location) as? SKSpriteNode, touchedNode == arrowDownLocks[i] {
+                    for j in 0..<10{
+                        if lockNumbers[i].texture?.description.components(separatedBy: "'")[1] == "lock0"{
+                            lockNumbers[i].texture = SKTexture(imageNamed: "lock9")
+                            break
+                        }else if lockNumbers[i].texture?.description.components(separatedBy: "'")[1] == "lock\(j)"{
+                            lockNumbers[i].texture = SKTexture(imageNamed: "lock\(j-1)")
+                            break
+                        }
+                    }
+                }
+            }
+            
+            if let touchedNode = atPoint(location) as? SKSpriteNode, touchedNode == confirmLock {
+                if(lockNumbers[0].texture?.description.components(separatedBy: "'")[1] == "lock9" && lockNumbers[1].texture?.description.components(separatedBy: "'")[1] == "lock9" && lockNumbers[2].texture?.description.components(separatedBy: "'")[1] == "lock9" && lockNumbers[3].texture?.description.components(separatedBy: "'")[1] == "lock9" ){
+                    print("success")
+                }else{
+                    print("gagal")
+                }
+            }
+        }
+    }
+    
+}
---
+++ Mini2/Scene1.swift
@@ -0,0 +1,101 @@
+//
+//  Scene1.swift
+//  Mini2
+//
+//  Created by beni garcia on 20/06/23.
+//
+
+import SpriteKit
+
+class Scene1: SKScene, SKPhysicsContactDelegate{
+    var player: SKSpriteNode?
+    var note: SKSpriteNode?
+    var noteZoom: SKSpriteNode?
+    var lastTouch: CGPoint? = nil
+    var isMovingLeft = false
+    var isMovingRight = false
+    let playerSpeed: CGFloat = 200.0
+    var lastUpdateTime: TimeInterval = 0
+    let playSoundAction = SKAction.playSoundFileNamed("sound_note", waitForCompletion: false)
+    var walkAnimation: SKAction?
+    
+    override func didMove(to view: SKView) {
+        note = childNode(withName: "note") as? SKSpriteNode
+        player = childNode(withName: "player") as? SKSpriteNode
+//        let noteZoomPosition = CGPoint(x: size.width / 2, y: size.height / 2)
+        noteZoom?.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        
+        
+        var walkFrames: [SKTexture] = []
+        
+        // Memuat gambar sprite berjalan
+        for i in 1...5 {
+            let texture = SKTexture(imageNamed: "avatar\(i)")
+            walkFrames.append(texture)
+        }
+        
+        // Membuat animasi berjalan
+        let walkAnimation = SKAction.animate(with: walkFrames, timePerFrame: 0.1)
+        self.walkAnimation = SKAction.repeatForever(walkAnimation)
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            if let touchedNode = atPoint(location) as? SKSpriteNode, touchedNode == note {
+                print("kepencet")
+                self.run(playSoundAction)
+            }
+            if !player!.contains(location) {
+                // Memeriksa posisi sentuhan pada pemain untuk menentukan arah pergerakan
+                if location.x < player!.position.x {
+                    isMovingLeft = true
+                    player?.run(walkAnimation!, withKey: "walking")
+                } else {
+                    isMovingRight = true
+                    player?.run(walkAnimation!, withKey: "walking")
+                }
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isMovingLeft = false
+        isMovingRight = false
+        player?.removeAction(forKey: "walking")
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Menghitung deltaTime
+        if lastUpdateTime == 0 {
+            lastUpdateTime = currentTime
+        }
+        let deltaTime = currentTime - lastUpdateTime
+        lastUpdateTime = currentTime
+        
+        // Update posisi pemain berdasarkan arah pergerakan yang ditentukan
+        if isMovingLeft {
+            movePlayerLeft(deltaTime: deltaTime)
+        } else if isMovingRight {
+            movePlayerRight(deltaTime: deltaTime)
+        }
+    }
+    
+    func movePlayerLeft(deltaTime: TimeInterval) {
+        let newPosition = CGPoint(x: player!.position.x - playerSpeed * CGFloat(deltaTime), y: player!.position.y)
+        
+        // Memeriksa batasan pergerakan ke kiri (misalnya, tepi layar atau area tertentu)
+        player?.position = newPosition
+        
+    }
+    
+    func movePlayerRight(deltaTime: TimeInterval) {
+        let newPosition = CGPoint(x: player!.position.x + playerSpeed * CGFloat(deltaTime), y: player!.position.y)
+        
+        // Memeriksa batasan pergerakan ke kanan (misalnya, tepi layar atau area tertentu)
+        player?.position = newPosition
+        
+    }
+}
