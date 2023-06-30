d533721b6d58389c0fe308804a591a87b790d630
Beni Garcia <benigarcia853@gmail.com>
2023-06-30 09:49:33 +0700

    trans

--- Mini2/ContentView.swift
+++ Mini2/ContentView.swift
@@ -8,10 +8,12 @@
 import Foundation
 import SwiftUI
 import SpriteKit
+import AVFoundation
 
 class GameData : ObservableObject {
     @Published var isPopUpVisible = false //scene
     @Published var isSecondPopUpVisible = false //image
+    
     
     @Published var popUpName = ""
     
@@ -23,6 +25,48 @@ class GameData : ObservableObject {
     @Published var windowSprite: SKSpriteNode?
     
     static let shared = GameData()
+    
+    func transitionScene(scene: SKScene, toScene: String) {
+        let transitionScene = SKScene(fileNamed: toScene)
+        transitionScene!.scaleMode = scene.scaleMode
+
+        
+        let fadeInAction = SKAction.fadeIn(withDuration: 0.5)
+        scene.run(fadeInAction)
+                     
+        let transition = SKTransition.fade(withDuration: 1.5)
+        scene.view?.presentScene(transitionScene!, transition: transition)
+    }
+    
+    func playVideo(scene: SKScene,videoName: String, videoExt: String, xPos: Double, durationVideo: Double, toScene: String){
+        // Create an AVPlayerItem
+        let videoURL = Bundle.main.url(forResource: videoName, withExtension: videoExt)!
+        let playerItem = AVPlayerItem(url: videoURL)
+
+        // Create an AVPlayer
+        let player = AVPlayer(playerItem: playerItem)
+
+        // Create an SKVideoNode with the AVPlayer
+        let videoNode = SKVideoNode(avPlayer: player)
+
+        // Set the video node's size and position
+//        videoNode.size = CGSize(width: 2732, height: 2048)
+        videoNode.position = CGPoint(x: xPos, y: 0)
+        videoNode.zPosition = 2
+        
+        // Add the video node to the scene
+        scene.addChild(videoNode)
+
+        // Play the video
+        
+        player.play()
+        DispatchQueue.main.asyncAfter(deadline: .now() + durationVideo) {
+            videoNode.removeFromParent()
+            self.transitionScene(scene: scene, toScene: toScene)
+        }
+        
+        
+    }
 }
 
 
